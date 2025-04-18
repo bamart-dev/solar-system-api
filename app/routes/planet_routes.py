f9aa17a87f19faeb00b8aede96750ffe150f5153
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-04-18 11:41:21 -0700

    implemented wave 2

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.planets import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
@@ -12,7 +12,27 @@ def get_all_planets():
             id = planet.id,
             name = planet.name,
             description = planet.description,
-            atmosphere = planet.atmosphere
+            atmosphere = planet.atmosphere,
         ))
 
     return results_list
+
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        return {"message": f"planet ({planet_id}) is invalid"}, 400
+
+    for planet in planets:
+        if planet_id == planet.id:
+            return {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "atmosphere": planet.atmosphere,
+            }
+
+    # return {"message": f"planet ({planet_id}) not found"}, 404
+    response = {"message": f"planet ({planet_id}) not found"}
+    abort(make_response(response, 404))
