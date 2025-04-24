3c4300c3686415a2aba40222c48e561a1c164b47
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-04-24 13:18:28 -0700

    commented out original code (prep for wave 3)

--- app/models/planets.py
+++ app/models/planets.py
@@ -1,16 +1,16 @@
 
 
-class Planet:
-    def __init__(self, id, name, description, atmosphere):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.atmosphere = atmosphere
+# class Planet:
+#     def __init__(self, id, name, description, atmosphere):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.atmosphere = atmosphere
 
-planets = [
-    Planet(1, "Cat Head Planet", "cat head shaped planet with large red spots", "cat's bathhouse cafe"),
-    Planet(2, "Clown Planet", "clown shaped planet", "scary yet fun and full of balloons"),
-    Planet(3, "Donut Planet", "donut shaped planet with a black hole in the center", "non-existant"),
-    Planet(4, "Jelly Cube", "perfect pink cube shaped planet with a sugared cherry at the bottom", "cannibalistic"),
-    Planet(5, "Palm Planet", "shaped like a large etheral palm with its fingers folded upward", "surface is a mixtrue of blue and purple gas")
-]
+# planets = [
+#     Planet(1, "Cat Head Planet", "cat head shaped planet with large red spots", "cat's bathhouse cafe"),
+#     Planet(2, "Clown Planet", "clown shaped planet", "scary yet fun and full of balloons"),
+#     Planet(3, "Donut Planet", "donut shaped planet with a black hole in the center", "non-existant"),
+#     Planet(4, "Jelly Cube", "perfect pink cube shaped planet with a sugared cherry at the bottom", "cannibalistic"),
+#     Planet(5, "Palm Planet", "shaped like a large etheral palm with its fingers folded upward", "surface is a mixtrue of blue and purple gas")
+# ]
--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -3,36 +3,42 @@ from app.models.planets import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-@planets_bp.get("")
-def get_all_planets():
-    results_list = []
+# @planets_bp.get("")
+# def get_all_planets():
+#     results_list = []
 
-    for planet in planets:
-        results_list.append(dict(
-            id = planet.id,
-            name = planet.name,
-            description = planet.description,
-            atmosphere = planet.atmosphere,
-        ))
+#     for planet in planets:
+#         results_list.append(dict(
+#             id = planet.id,
+#             name = planet.name,
+#             description = planet.description,
+#             atmosphere = planet.atmosphere,
+#         ))
 
-    return results_list
+#     return results_list
 
-@planets_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        return {"message": f"planet ({planet_id}) is invalid"}, 400
+# @planets_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
 
-    for planet in planets:
-        if planet_id == planet.id:
-            return {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "atmosphere": planet.atmosphere,
-            }
+#     return {
+#         "id": planet.id,
+#         "name": planet.name,
+#         "description": planet.description,
+#         "atmosphere": planet.atmosphere,
+#     }
 
-    # return {"message": f"planet ({planet_id}) not found"}, 404
-    response = {"message": f"planet ({planet_id}) not found"}
-    abort(make_response(response, 404))
+
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         message = {"mesage": f"planet ({planet_id}) is invalid"}
+#         abort(make_response(message, 400))
+
+#     for planet in planets:
+#         if planet_id == planet.id:
+#             return planet
+
+#     response = {"message": f"planet ({planet_id}) not found"}
+#     abort(make_response(response, 404))
