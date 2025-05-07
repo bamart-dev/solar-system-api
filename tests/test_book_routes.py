66a7d4c2d11703f92609b8e19b84fe37c68e721b
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-05-07 11:15:06 -0700

    implement additional tests (at 98% coverage)

--- tests/test_book_routes.py
+++ tests/test_book_routes.py
@@ -1,3 +1,5 @@
+from app.db import db
+from app.models.planets import Planet
 
 
 def test_get_one_planet(client, create_two_planets):
@@ -20,6 +22,16 @@ def test_get_one_planet_no_data(client):
     assert response.status_code == 404
     assert response_body == {
         "message": "planet (1) not found",
+    }
+
+
+def test_get_one_planet_invalid_data(client):
+    response = client.get("/planets/%^&")
+    resonse_body = response.get_json()
+
+    assert response.status_code == 400
+    assert resonse_body == {
+        "message": "planet (%^&) is invalid",
     }
 
 
@@ -56,6 +68,14 @@ def test_get_all_planets(client, replicate_development_db):
     ]
 
 
+def test_get_all_planets_no_data(client):
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == []
+
+
 def test_create_one_planet(client):
     response = client.post("/planets", json={
         "name": "Coruscant",
@@ -70,4 +90,77 @@ def test_create_one_planet(client):
         "name": "Coruscant",
         "description": "A city-covered planet; capital of a galaxy",
         "atmosphere": "Vibrant, very loud",
+    }
+
+
+def test_update_planet(client, create_two_planets):
+    response = client.put("/planets/1", json={
+        "name": "Dagobah",
+        "description": "remote world full of swamps and forests",
+        "atmosphere": "pure",
+    })
+    query = db.select(Planet).where(Planet.id == 1)
+    updated_planet = db.session.scalar(query)
+
+    assert response.status_code == 204
+    assert updated_planet.name == "Dagobah"
+    assert updated_planet.description == "remote world full of swamps and forests"
+    assert updated_planet.atmosphere == "pure"
+
+
+def test_update_planet_missing_planet(client):
+    response = client.put("/planets/1", json={
+        "name": "Dagobah",
+        "description": "remote world full of swamps and forests",
+        "atmosphere": "pure",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {
+        "message": "planet (1) not found",
+    }
+
+
+def test_update_planet_invalid_id(client, create_two_planets):
+    response = client.put("/planets/!", json={
+        "name": "Dagobah",
+        "description": "remote world full of swamps and forests",
+        "atmosphere": "pure",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {
+        "message": "planet (!) is invalid",
+    }
+
+
+def test_delete_planet(client, create_two_planets):
+    response = client.delete("/planets/1")
+
+    query = db.select(Planet).where(Planet.id == 1)
+    deleted_planet = db.session.scalar(query)
+
+    assert response.status_code == 204
+    assert not deleted_planet
+
+
+def test_delete_planet_missing_planet(client):
+    response = client.delete("/planets/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {
+        "message": "planet (1) not found",
+    }
+
+
+def test_delete_planet_invalid_id(client, create_two_planets):
+    response = client.delete("/planets/three")
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {
+        "message": "planet (three) is invalid",
     }
